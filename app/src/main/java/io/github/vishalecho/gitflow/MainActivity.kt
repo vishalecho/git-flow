2b7edd11484a1ae0d170cb18e10787046dc02e9f
vishalecho <vkr1994@gmail.com>
2020-06-12 13:48:30 +0530

    First commit

---
+++ app/src/main/java/io/github/vishalecho/gitflow/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.vishalecho.gitflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
